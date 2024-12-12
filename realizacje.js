bdcaaf335ee2e4a2c129796ad27aa35b6eb89ccf
sturioo <shopify.igor10@gmail.com>
2024-12-12 15:08:13 +0100

    Create realizacje.js

---
+++ realizacje.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+// Close menu when clicking outside
+document.addEventListener('click', (e) => {
+    if (!nav.contains(e.target) && !hamburger.contains(e.target)) {
+        nav.classList.remove('active');
+    }
+});
